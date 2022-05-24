80557583af14d06d92aed21362372d1c5eff931f
Alexander Gonzalez <alex_gonzalez@ultimatesoftware.com>
2022-05-24 18:41:54 -0400

    Save changes from simple use or maintance

--- start-app.cmd
+++ start-app.cmd
@@ -3,5 +3,5 @@
 cd C:\Projects\text-parser\Sources\TextParser\
 call dotnet build
 cd C:\Projects\text-parser\Sources\TextParser\bin\Debug\netcoreapp3.1\
-call TextParser.exe "C:\\Projects\\dev-refs\\Scripts\\utils\\" "kindleHighlightsSource.txt" "C:\\Projects\\dev-refs\\Scripts\\utils\\" kindleHighlightsOutput.md
+call TextParser.exe "C:\\Projects\\dev-refs\\Scripts\\utils\\kindle-highlights\\" "kindleHighlightsSource.txt" "C:\\Projects\\dev-refs\\Scripts\\utils\\kindle-highlights\\" kindleHighlightsOutput.md
 cd C:\Projects\text-parser\
